3b98a00d8c270605c046a46509750881d9a312aa
xuezhang <shirly_yukizhang@hotmail.com>
2020-12-21 18:11:36 -0800

    1. Creat login page and register page 2. Implement login function 3. Connect with back-end to get token

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,30 @@ import '../styles/App.css';
 import TopBar from './TopBar';
 import Main from './Main';
 import React, {useState} from "react";
+import {TOKEN_KEY} from '../constants';
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        localStorage.getItem(TOKEN_KEY) ? true : false
+    );
     const logout = () => {
         console.log("logout");
+        localStorage.removeItem(TOKEN_KEY);
         setIsLoggedIn(false);
     }
+
+    const loggedIn = (token) => {
+        //console.log('token -> ', token);
+        if (token) {
+            localStorage.setItem(TOKEN_KEY, token);
+            setIsLoggedIn(true);
+        }
+    }
+
   return (
     <div className="App">
       <TopBar isLoggedIn={isLoggedIn} handleLogout={logout}/>
-      <Main/>
+      <Main isLoggedIn={isLoggedIn} handleLoggedIn={loggedIn}/>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
 
 function Main(props) {
+    const{ isLoggedIn, handleLoggedIn } = props;
+    // console.log(handleLoggedIn);
+    const showLogin = () => {
+        //case 1: already logged in
+        //case 2: not logged in yet
+        return isLoggedIn ? <Redirect to="/home" /> : <Login handleLoggedIn = {handleLoggedIn}/>
+    };
+
+    const showHome = () => {
+        //logged in ?
+        return isLoggedIn ? <Home /> : <Redirect to="/login" />
+
+    };
+
     return (
-        <div className="main">Main</div>
+        <div className="main">
+            <Switch>
+                <Route path="/login" render={showLogin}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/home" render={showHome}/>
+            </Switch>
+        </div>
     );
 }
 
